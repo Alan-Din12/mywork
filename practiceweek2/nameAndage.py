79cea0a7a1e06f57b894d5420b22045d1b7c0959
Alan Dineen <alandineen@Alans-MacBook-Pro.local>
2023-02-08 14:37:36 +0000

    NameandAge

---
+++ practiceweek2/nameAndage.py
@@ -0,0 +1,9 @@
+#nameAndage.py
+#Author : Alan Dineen
+#This program will print out "Hello + name + your age"
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+
+print('Hello ' + name + ',\t your age is ' + age)
+
