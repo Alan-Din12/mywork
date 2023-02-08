8bb07b2aaad78ba52776626df599c32e41e54cbc
Alan Dineen <alandineen@Alans-MacBook-Pro.local>
2023-02-08 14:27:14 +0000

    Hello2

---
+++ practiceweek2/Hello2.py
@@ -0,0 +1,7 @@
+#Hello2.py
+#Author : Alan Dineen
+#This program will print out "Hello and your name, and on a new line "nice to meet you"
+
+name = input("Enter your name:")
+print ('Hello '  + name + '\n nice to meet you')
+
