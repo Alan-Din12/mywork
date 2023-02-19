2705b678a0a27a4da4d395dd3348f205385d8756
Alan Dineen <alandineen@Alans-MacBook-Pro.local>
2023-02-19 21:30:02 +0000

    Collatz Conjecture Problem

---
+++ Week4/collatz.py
@@ -0,0 +1,27 @@
+#Collatz practice
+#
+#Author : Alan Dineen
+
+
+def collatz(x):
+    while x != 1:
+        if x % 2 > 0:
+             x =((3 * x) + 1)
+             list_.append(x)
+        else:
+            x = (x / 2)
+            list_.append(x)
+    return list_
+print('Please enter a number: ', end='')
+while True:
+    try:
+        x = int(input())
+        list_ = [x]
+        break
+    except ValueError:
+        print('Invaid selection, try again: ', end='')
+
+l = collatz(x)
+
+print('\nList:', l, sep=' ')
+print('Number of steps required:', len(l) - 1)
